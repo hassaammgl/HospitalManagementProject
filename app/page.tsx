aa75b101d13a4eb65359e8fbb48ebc003569b13d
hassaammgl <h455a4mmehtab@gmail.com>
2024-07-14 22:30:14 +0500

    adding db models

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,28 @@
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
-import { Button } from "@/components/ui/button";
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-80 h-80 bg-pink-400 sm:bg-red-500 lg:bg-green-500 md:bg-yellow-500 xl:bg-blue-500 2xl:bg-black"></div>
-      <Button>Hassaam</Button>
-      <Drawer>
-        <DrawerTrigger>Open</DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+    <main className="h-screen w-screen">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="rounded border border-slate-700"
+      >
+        <ResizablePanel defaultSize={25} maxSize={25} minSize={7}>
+          <div className="flex h-full w-full items-center justify-center p-6">
+            <span className="font-semibold">Sidebar</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={75}>
+          <div className="flex w-full h-full items-center justify-center p-6">
+            <span className="font-semibold">Content</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </main>
   );
 }
